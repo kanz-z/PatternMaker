8fa097a8627ad588c23bd6c7326c81185ca3923e
Kanza <131506375+kanz-z@users.noreply.github.com>
2026-01-01 16:52:15 +0700

    Replace '*' with '* ' in pattern functions

--- patterns.py
+++ patterns.py
@@ -2,25 +2,25 @@ def make_pyramid(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "*" * (2*i - 1))
+        lines.append(" " * spasi + "* " * (2*i - 1))
     return "\n".join(lines)
 
 def make_segitiga_kiri(x):
-    return "\n".join("*" * i for i in range(1, x + 1))
+    return "\n".join("* " * i for i in range(1, x + 1))
 
 def make_segitiga_kanan(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "*" * i)
+        lines.append(" " * spasi + "* " * i)
     return "\n".join(lines)
 
 def make_belah_ketupat(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "*" * (2*i - 1))
+        lines.append(" " * spasi + "* " * (2*i - 1))
     for i in range(x - 1, 0, -1):
         spasi = x - i
-        lines.append(" " * spasi + "*" * (2*i - 1))
+        lines.append(" " * spasi + "* " * (2*i - 1))
     return "\n".join(lines)
