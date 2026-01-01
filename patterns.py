6cc5affc1b0d78da42a2ab67e992113b2cf6b68b
Kanza <131506375+kanz-z@users.noreply.github.com>
2026-01-01 16:54:18 +0700

    Fix spacing in pyramid and triangle functions

--- patterns.py
+++ patterns.py
@@ -2,7 +2,7 @@ def make_pyramid(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "* " * (2*i - 1))
+        lines.append("  " * spasi + "* " * (2*i - 1))
     return "\n".join(lines)
 
 def make_segitiga_kiri(x):
@@ -12,15 +12,15 @@ def make_segitiga_kanan(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "* " * i)
+        lines.append("  " * spasi + "* " * i)
     return "\n".join(lines)
 
 def make_belah_ketupat(x):
     lines = []
     for i in range(1, x + 1):
         spasi = x - i
-        lines.append(" " * spasi + "* " * (2*i - 1))
+        lines.append("  " * spasi + "* " * (2*i - 1))
     for i in range(x - 1, 0, -1):
         spasi = x - i
-        lines.append(" " * spasi + "* " * (2*i - 1))
+        lines.append("  " * spasi + "* " * (2*i - 1))
     return "\n".join(lines)
